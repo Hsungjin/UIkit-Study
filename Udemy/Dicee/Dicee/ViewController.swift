f60453c0ca51e4dd37c3a7b2682030db43f8196a
Hsungjin <hsungjin142@gmail.com>
2024-04-20 19:09:42 +0900

    Udemy-Uikit Dicee

---
+++ Udemy/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by 황성진 on 4/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
