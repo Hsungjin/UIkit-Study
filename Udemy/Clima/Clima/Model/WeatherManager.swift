a4609a3b8b38741813487533c4e3bfc2dd56ad34
Hsungjin <hsungjin142@gmail.com>
2024-05-08 20:16:50 +0900

    Clima 강의 내용

---
+++ Udemy/Clima/Clima/Model/WeatherManager.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 황성진 on 5/8/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherApiKey
+    
+}
